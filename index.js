28d9f54c746971d53178ff92d5683c2d4390e0ca
Seong Kim <runmath@gmail.com>
2023-03-07 15:24:59 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, wording) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${wording} gift!`;
+    }
+
+    return messages;
+
+}
+
+function countDown(numberForCountingDown) {
+    let i = numberForCountingDown;
+    while (i >= 0) {
+        console.log(i);
+        i --;
+    }
+}
